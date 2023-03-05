62f060968a677be00bfbd9f4bbbbd3a8f8c4cb3e
sidi-abdallh <mohamed-el-mokhtar.sidi-abdallah@ecole.ensicaen.fr>
2023-03-05 15:33:49 +0100

    first activity

---
+++ app/src/main/java/com/ensicaen/calculatrice/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ensicaen.calculatrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn = (Button) findViewById(R.id.buttonCalculatrice);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent appel = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(appel);
+            }
+        });
+    }
+
+}
